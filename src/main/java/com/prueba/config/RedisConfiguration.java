ce0ac942dae5269724329903e38a606f3ab02590
jaria15 <jaria15@bancodebogota.com.co>
2021-11-29 11:34:59 -0500

    prueba redis 2

--- src/main/java/com/prueba/config/RedisConfiguration.java
+++ src/main/java/com/prueba/config/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.prueba.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -9,17 +10,30 @@ import com.prueba.model.Item;
 
 @Configuration
 public class RedisConfiguration {
-	
-	    @Bean
-	    JedisConnectionFactory jedisConnectionFactory() {
-	        return new JedisConnectionFactory();
-	    }
 
-	    @Bean
-	    RedisTemplate<String, Item> redisTemplate() {
-	        final RedisTemplate<String, Item> redisTemplate = new RedisTemplate<>();
-	        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-	        return redisTemplate;
-	    }
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.port}")
+	private int redisPort;
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		factory.setHostName(redisHost);
+		factory.setPort(redisPort);
+		factory.setUsePool(true);
+		factory.getPoolConfig().setMaxIdle(30);
+		factory.getPoolConfig().setMinIdle(10);
+		return factory;
+	}
+
+	@Bean
+	RedisTemplate<String, Item> redisTemplate() {
+		final RedisTemplate<String, Item> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
 
 }
